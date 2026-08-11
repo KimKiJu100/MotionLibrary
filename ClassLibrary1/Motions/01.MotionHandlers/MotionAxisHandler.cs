575f9615c0835dd3a63840f9b8c06e84a8b4c759
kkj <goodkkj96@gmail.com>
2026-08-11 13:32:53 +0900

    firstcommit

---
+++ ClassLibrary1/Motions/01.MotionHandlers/MotionAxisHandler.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1.Motions._01.MotionHandlers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Motions.MotionHandler
+{
+    /// <summary>
+    /// 축 핸들러 클래스입니다. 이 클래스는 모션 축을 처리하는 기능을 제공합니다.    
+    /// </summary>
+    public sealed class MotionAxisHandler : MotionHandlerBase , IMotionAxisHandler
+    {
+        public int AxisIndex { get; set; }
+        public MotionAxisHandler()
+        {
+                
+        }
+    }
+}
---
+++ ClassLibrary1/Motions/02.MotionConnections/Base/MotionConnectionBase.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1.Motions._01.MotionHandlers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Motions._02.MotionConnections.Base
+{
+    /// <summary>
+    /// 커넥션 기능을 제공하는 기본 클래스입니다. 이 클래스는 모션 커넥션의 공통 기능을 정의하며, 다른 커넥션 클래스에서 상속하여 사용할 수 있습니다.
+    /// </summary>
+    public class MotionConnectionBase
+    {
+        public MotionConnectionBase()
+        {
+        }
+    }
+}
