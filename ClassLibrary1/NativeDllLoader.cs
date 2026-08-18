abf54529246283981cfe27069b11c34cda8ebe10
kkj <goodkkj96@gmail.com>
2026-08-18 16:20:38 +0900

    1.MotionLibrary.dll 참조에 따라 Axl자동 생성

---
+++ ClassLibrary1/NativeDllLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class NativeDllLoader
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetDllDirectory(string lpPathName);
+
+        public static void Initialize()
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            string nativeDir = Environment.Is64BitProcess
+                ? Path.Combine(baseDir, "x64")
+                : Path.Combine(baseDir, "x86");
+
+            if (!SetDllDirectory(nativeDir))
+            {
+                throw new InvalidOperationException(
+                    $"Native DLL 경로 설정 실패: {nativeDir}");
+            }
+        }
+    }
+}
--- MotorTest/Program.cs
+++ MotorTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,6 +19,7 @@ namespace MotorTest
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
 
+            NativeDllLoader.Initialize();
             var bootstrapper = new AppBootstrapper();
             bootstrapper.Run();
         }
